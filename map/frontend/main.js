9bf4f955ecdd1b2daaab9c89600e49dd4bf0fe4b
truongmm <truongmm@gmail.com>
2015-09-20 02:14:00 -0700

    Revert use of beta

--- map/frontend/main.js
+++ map/frontend/main.js
@@ -1,65 +1,72 @@
-var map, view;
+var map;
 
-function addShelterMarker(graphicsLayer){
+function renderDisasterInfo(map, point) {
+  map.infoWindow.setTitle("TCDisrupt SF Fire");
+  map.infoWindow.setContent("HELPPP");
+
+  $("#alert .disaster").on("click", function(){
+    map.infoWindow.show(point);
+  });
+}
+
+function addDisasterMarker(map, graphicsLayer){
   require([
-    "esri/Graphic",
+    "esri/graphic",
     "esri/symbols/PictureMarkerSymbol",
-    "esri/geometry/Point",
-    "esri/widgets/PopupTemplate"
+    "esri/geometry/Point"
   ],
-  function(Graphic, PictureMarkerSymbol, Point, PopupTemplate) {
-    // Shelter is SF Public Library: Potrero Branch
-    var shelterPoint = new Point({
+  function(Graphic, PictureMarkerSymbol, Point) {
+    // Disaster is SF Public Library: Potrero Branch
+    var disasterPoint = new Point({
       "latitude": 37.7605734,
       "longitude": -122.3937939
     });
 
-    var shelterInfo = new PopupTemplate({
-      "title": "Shelter"
-    });
-
-    var shelterMarker = new PictureMarkerSymbol({
-      "height": 48,
+    // Initialize disaster marker
+    var disasterMarker = new PictureMarkerSymbol({
+      "height": 36,
       "url": "http://www2.psd100.com/ppp/2013/11/0501/Map-marker-icon-1105213652.png",
-      "width": 48
+      "width": 36
     });
+    var disasterGraphic = new Graphic(disasterPoint, disasterMarker);
 
-    var shelterGraphic = new Graphic({
-      "geometry": shelterPoint,
-      "popupTemplate": shelterInfo,
-      "symbol": shelterMarker
-    });
-
-    graphicsLayer.add(shelterGraphic);
+    graphicsLayer.add(disasterGraphic);
+    renderDisasterInfo(map, disasterPoint);
   });
 }
 
 function initialize(){
   require([
+    "esri/Color",
     "esri/layers/GraphicsLayer",
-    "esri/Map",
-    "esri/views/MapView",
+    "esri/dijit/InfoWindow",
+    "esri/map",
+    "esri/dijit/Popup",
+    "esri/symbols/SimpleFillSymbol",
+    "dojo/dom-construct",
     "dojo/domReady!"
-  ],
-  function(GraphicsLayer, Map, MapView) {
-    map = new Map({
-      basemap: "topo"
-    });
+    ], function(Color, GraphicsLayer, InfoWindow, Map, Popup, SimpleFillSymbol, domConstruct) {
 
-    // View is centered on the city of San Francisco
-    view = new MapView({
-      center: [-122.45, 37.75], //lat, long
-      container: "viewDiv",
-      map: map,
+    // Initialize info window
+    var fill = new SimpleFillSymbol("solid", null, new Color("#A4CE67"));
+    var infoWindow = new Popup({
+        fillSymbol: fill
+    }, domConstruct.create("div"));
+
+    // Map is centered on the city of San Francisco
+    map = new Map("map", {
+      basemap: "topo",
+      center: [-122.45, 37.75], // longitude, latitude
+      infoWindow: infoWindow,
       zoom: 12
     });
 
     // Initialize graphics layer
     var graphicsLayer = new GraphicsLayer();
-    map.add(graphicsLayer);
+    map.addLayer(graphicsLayer);
 
-    // Add shelter marker to map
-    addShelterMarker(graphicsLayer);
+    // Add disaster marker to map
+    addDisasterMarker(map, graphicsLayer);
   });
 }
 
